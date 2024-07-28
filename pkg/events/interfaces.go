2049caa561d3a9acd893f8aa62a0e8d450cfad5e
Tiago Sales <tiagosales@outlook.com>
2024-07-28 16:16:05 -0300

    Criando EventDispacherinterface

--- pkg/events/interfaces.go
+++ pkg/events/interfaces.go
@@ -7,3 +7,17 @@ type EventInterface interface{
 	GetDateTime() time.Time
 	GetPayload() interface{}
 }
+
+type EventHandlerInterface interface{
+	Handle(event EventInterface)
+}
+
+type EventDispatcherInterface interface{
+	Register(eventName string, handle EventHandlerInterface ) error
+	Dispatch(event EventInterface) error
+	Remove(eventName string, handler EventHandlerInterface) error
+	Has(eventName string, handler EventHandlerInterface) bool
+	Clear() error
+}
+
+
